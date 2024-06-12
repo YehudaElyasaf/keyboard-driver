49d31e4ff69ecab47454c8af079e735abd325d31
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2024-06-13 00:53:29 +0300

    init exit driver

---
+++ driver.c
@@ -0,0 +1,15 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init init1(void){
+	printk(KERN_INFO "Hi!\n");
+	return 0;
+}
+
+static void __exit exit1(void){
+	printk(KERN_INFO "Bye!\n");
+}
+
+module_init(init1);
+module_exit(exit1);
