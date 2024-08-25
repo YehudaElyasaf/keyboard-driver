f16d2707f126f369569275c8d1aaa45a3a3ef470
Yehuda Elyasaf <30yehuda26@gmail.com>
2024-08-26 01:04:14 +0300

    split headers to keyboard_driver.h and usb.h

--- keyboard_driver.c
+++ keyboard_driver.c
@@ -1,4 +1,5 @@
 #include "keyboard_driver.h"
+#include "usb.h"
 
 
 static int __init keyboard_driver_init(void){
@@ -12,14 +13,6 @@ static int __init keyboard_driver_init(void){
 		printk(KERN_ERR MODULE_PRINT_PREFIX "Couldn't load USB device. Error code: %d\n", result);
 		return -1;
 	}
-
-	//request irq
-	result = request_irq(KEY_IRQ, key_irq_handler, IRQF_SHARED, MODULE_NAME, (void*) key_irq_handler);
-	if(result != 0){
-                //invalid result code
-                printk(KERN_ERR MODULE_PRINT_PREFIX "Couldn't register irq handler. Error code: %d\n", result);
-                return -1;
-        }
 	
 	printk(KERN_INFO MODULE_PRINT_PREFIX "Driver initialized successfully!\n");
 	return 0;
@@ -31,9 +24,6 @@ static void __exit keyboard_driver_exit(void)
 
 	//deregister USB device
 	usb_deregister(&usb_keyboard_driver);
-
-	//free irq
-	free_irq(KEY_IRQ, NULL);
 
 	printk(KERN_INFO MODULE_PRINT_PREFIX "Driver exited successfully!\n");
 }
@@ -47,9 +37,4 @@ static int usb_probe(struct usb_interface* interface, const struct usb_device_id
 
 static void usb_disconnect(struct usb_interface* interface){
 	printk(KERN_INFO MODULE_PRINT_PREFIX "USB device disconnected\n");
-}
-
-static irqreturn_t key_irq_handler(int irq, void* dev_id){
-	printk(KERN_INFO MODULE_PRINT_PREFIX "IRQ!");
-	return IRQ_HANDLED;
 }
--- keyboard_driver.h
+++ keyboard_driver.h
@@ -1,42 +1,21 @@
+#pragma once
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/usb.h>
-#include <linux/interrupt.h>
 
 #define MODULE_NAME		"keyboard_driver"
 #define MODULE_PRINT_PREFIX	MODULE_NAME ": "
-
-#define VENDOR_ID		0x03f0
-#define PRODUCT_ID		0x0024
-
-#define KEY_IRQ			1
 
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("USB keyboard driver for Linux");
 MODULE_AUTHOR("Yehuda Elyasaf");
 
-static struct usb_device_id usb_id_table[] = {
-	// TODO: use USE_DEVICE_INFO INSTED? (for compatibility)
-	{ USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
-	{ },
-};
-MODULE_DEVICE_TABLE(usb, usb_id_table);
-
 
 static int __init keyboard_driver_init(void);
 static void __exit keyboard_driver_exit(void);
-static int usb_probe(struct usb_interface* interface, const struct usb_device_id* id);
-static void usb_disconnect(struct usb_interface* interface);
-static irqreturn_t key_irq_handler(int irq, void* dev_id);
-
-static struct usb_driver usb_keyboard_driver = {
-	.name		= MODULE_NAME,
-	.probe		= usb_probe,
-	.disconnect	= usb_disconnect,
-	.id_table	= usb_id_table,
-};
 
 module_init(keyboard_driver_init);
 module_exit(keyboard_driver_exit);
